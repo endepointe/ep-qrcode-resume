235c7221008265d3a166e518c0d38a893b8b5110
endepointe <endepointe@gmail.com>
2025-02-26 19:05:19 -0800

    verification and naming change for clarity

--- rsa_methods.py
+++ rsa_methods.py
@@ -3,6 +3,7 @@ import base64
 
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec, rsa, padding
+from cryptography.exceptions import InvalidSignature
 
 if len(sys.argv) < 3:
     print(f"\nUsage: python {sys.argv[0]} <signing_message> <plain_text>\n")
@@ -11,32 +12,32 @@ if len(sys.argv) < 3:
 '''
 Generates a tuple after signing with a message. 
 '''
-def generate_rsa(message: str) -> (rsa.RSAPrivateKey,rsa.RSAPublicKey, bytes):
+def generate_rsa(signing_message: str) -> (rsa.RSAPrivateKey, bytes):
     private_key = rsa.generate_private_key(65537,4096)
     signature = private_key.sign(
-                    message.encode('utf-8'),
+                    signing_message.encode('utf-8'),
                     padding.PSS(
                         mgf=padding.MGF1(hashes.SHA256()),
                         salt_length=padding.PSS.MAX_LENGTH
                     ),
                     hashes.SHA256()
                 )
-    return (private_key,private_key.public_key(),base64.b64encode(signature))
+    return (private_key,signature)
 
 '''
 Encrypt a plaintext with public key
 secret is type: bytes. base64 encode so it can be used in qr code url.
 '''
-def encrypt_secret(public_key: rsa.RSAPublicKey, message: str) -> bytes:
+def encrypt_secret(public_key: rsa.RSAPublicKey, plain_text: str) -> bytes:
     secret = public_key.encrypt(
-                                message.encode('utf-8'),
+                                plain_text.encode('utf-8'),
                                 padding.OAEP(
                                     mgf=padding.MGF1(algorithm=hashes.SHA256()),
                                     algorithm=hashes.SHA256(),
                                     label=None
                                 )
                             )
-    return base64.b64encode(secret)
+    return secret
 
 def decrypt_ciphertext(private_key: rsa.RSAPrivateKey, ciphertext: str) -> bytes:
     plaintext = private_key.decrypt(
@@ -49,16 +50,39 @@ def decrypt_ciphertext(private_key: rsa.RSAPrivateKey, ciphertext: str) -> bytes
                 )
     return plaintext 
 
+def verify_signature(private_key: rsa.RSAPrivateKey, signature: bytes, signing_message: str) -> bool:
+    try:
+        public_key = private_key.public_key()
+        public_key.verify(
+                    signature,
+                    signing_message.encode('utf-8'),
+                    padding.PSS(
+                        mgf=padding.MGF1(hashes.SHA256()),
+                        salt_length=padding.PSS.MAX_LENGTH
+                    ),
+                    hashes.SHA256()
+        )
+        return True
+    except InvalidSignature as ISE:
+        print(f"ISE: {ISE}")
+        return False
+    except Exception as E:
+        print(f"some error occurred: {E}")
+
+
 keys = generate_rsa(str(sys.argv[1]))
 rsa_private_key = keys[0]
-rsa_public_key = keys[1] 
-signature = keys[2] 
+rsa_public_key = rsa_private_key.public_key() 
+signature = keys[1] 
 ciphertext = encrypt_secret(rsa_public_key, str(sys.argv[2]))
-plaintext = decrypt_ciphertext(rsa_private_key, base64.b64decode(ciphertext))
+plaintext = decrypt_ciphertext(rsa_private_key, ciphertext)
 
-print(f"\nCiphertext: {ciphertext.decode()}\n")
-print(f"\nSignature: {signature.decode()}\n")
+print(f"\nCiphertext: {ciphertext}\n")
+print(f"\nSignature: {signature}\n")
 
 # Correctness check
 assert(plaintext.decode() == str(sys.argv[2]))
 print(f"{plaintext.decode()} == {sys.argv[2]}")
+r = verify_signature(rsa_private_key, signature, str(sys.argv[1])),
+print(f"r: {r}")
+
